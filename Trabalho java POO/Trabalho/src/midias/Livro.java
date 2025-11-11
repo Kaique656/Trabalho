5a1cc75ae5b03603ef2608c68ba3dde35ab5ec77
Kaique656 <cunhakaique004@gmail.com>
2025-11-11 14:35:17 -0300

    Add files via upload

---
+++ Trabalho java POO/Trabalho/src/midias/Livro.java
@@ -0,0 +1,28 @@
+package midias;
+
+public class Livro extends Pasta {
+
+	private String autores;
+	
+	public Livro(String titulo, String categoria, double duracao,String autores,String nomeArquivo) {
+		super(titulo, categoria, duracao,nomeArquivo);
+		setAutores(autores);
+	}
+	
+	public String getAutores() {
+		return autores;
+	}
+	public void setAutores(String autores) {
+		this.autores = autores;
+	}
+	
+	@Override
+	public String mostrar() {
+	    return "Título: " + getTitulo() + ", Categoria: " + getCategoria() +
+	           ", Páginas: " + getDuracao() + ", Autor: " + getAutores();
+	}
+
+
+	
+
+}
